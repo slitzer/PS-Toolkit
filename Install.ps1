feee2b902c98fffebb8c42744c507a8aaca7958d
slitzer <slitzer@gmail.com>
2026-02-21 13:08:46 +1300

    Modify Install.ps1 for PS-Toolkit integration
    Updated installation script to use PS-Toolkit repository and added NetworkTest plugin.

--- Install.ps1
+++ Install.ps1
@@ -1,5 +1,11 @@
-$AppName="PSAudioTools";$AppPath="C:\$AppName"
-New-Item "$AppPath\Plugins" -ItemType Directory -Force|Out-Null
-irm "https://raw.githubusercontent.com/YOURUSER/PSAudioTools/main/UI.ps1" -OutFile "$AppPath\UI.ps1"
-irm "https://raw.githubusercontent.com/YOURUSER/PSAudioTools/main/Plugins/AudioToMP3.ps1" -OutFile "$AppPath\Plugins/AudioToMP3.ps1"
+# Install.ps1 - updated for https://github.com/slitzer/PS-Toolkit.git
+
+$AppName = "PSToolkit"
+$AppPath = "C:\$AppName"
+New-Item "$AppPath\Plugins" -ItemType Directory -Force | Out-Null
+
+irm "https://raw.githubusercontent.com/slitzer/PS-Toolkit/main/UI.ps1" -OutFile "$AppPath\UI.ps1"
+irm "https://raw.githubusercontent.com/slitzer/PS-Toolkit/main/Plugins/AudioToMP3.ps1" -OutFile "$AppPath\Plugins/AudioToMP3.ps1"
+irm "https://raw.githubusercontent.com/slitzer/PS-Toolkit/main/Plugins/NetworkTest.ps1" -OutFile "$AppPath\Plugins/NetworkTest.ps1"
+
 & "$AppPath\UI.ps1"
